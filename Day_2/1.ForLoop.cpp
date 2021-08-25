345f72afbacd8bb3e610d29e257a126b6b0440a0
aman <caman411@gmail.com>
2021-08-25 09:34:39 +0530

    adding loop

---
+++ Day_2/1.ForLoop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n;
+    int i;
+
+    cout<<"how many times you want to print hello world?\n";
+    cin>>n;
+
+     for ( i = 1; i <= n; i++)
+     {
+        cout<<"hello world \n";
+     }
+     
+
+    return 0;
+}
