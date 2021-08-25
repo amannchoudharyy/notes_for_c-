9a78291d00cb62180aee4b51968cad09dcf41f8a
aman <caman411@gmail.com>
2021-08-26 01:14:25 +0530

    formatting files

--- Day_2/5.continue.cpp
+++ Day_2/5.continue.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
-int main(){
+int main()
+{
 
-     for(int i=0;i<100;i++){
-         if (i%3==0)
-         {
-           continue;
-         }
-         cout<<i<<"  ";
-     }
+  for (int i = 0; i < 100; i++)
+  {
+    if (i % 3 == 0)
+    {
+      continue;
+    }
+    cout << i << "  ";
+  }
 
-    return 0;
- 
+  return 0;
 }
--- Day_2/6.break.cpp
+++ Day_2/6.break.cpp
@@ -1,23 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
 
-    int n,i;
-    cin>>n;
+    int n, i;
+    cin >> n;
 
     for (i = 2; i < n; i++)
     {
-       if(n%i==0){
-            cout<<"number is non prime";
+        if (n % i == 0)
+        {
+            cout << "number is non prime";
             break;
-       }
-
+        }
     }
-    if(i==n){
-        cout<<"number is prime";
+    if (i == n)
+    {
+        cout << "number is prime";
     }
-    
-
 
     return 0;
 }
