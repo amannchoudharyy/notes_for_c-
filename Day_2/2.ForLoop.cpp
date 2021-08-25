400a0074a4ac9224167b5cfaa44696a0c4fd9c00
aman <caman411@gmail.com>
2021-08-25 19:52:10 +0530

    addding second FOR loop

---
+++ Day_2/2.ForLoop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+  int main(){
+
+      int a,b,c;
+
+      cin>>a;
+       b=0;
+
+      for(c=0;c<=a;c++){
+          b = b+c;
+      }
+      
+      cout<<b;
+
+    return 0;
+   }
