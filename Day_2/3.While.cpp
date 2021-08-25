b748fd1be8e11b2e9dc425630998ea85fbe212c0
aman <caman411@gmail.com>
2021-08-25 21:27:24 +0530

    adding while and do while loops

---
+++ Day_2/3.While.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"enter a number ";
+    cin>>a;
+     
+     while (a>0)
+     {
+         cout<<a<<endl;
+
+         cin>>a;
+     }
+    
+    cout<<"number is negative";
+     
+    return 0;
+}
